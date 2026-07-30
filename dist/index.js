a07e7e30e089e06a005737276927c5afe31dfc6f
yuri_rodnov <somewaystodo@yandex.ru>
2026-07-30 09:30:38 +0300

    change structure

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
-"use strict";
-console.log("hello");
+import express from "express";
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.get("/data", (req, res) => { });
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import express from "express";
+export const app = express();
+export const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.get("/data", (req: Request, res: Response) => {});
