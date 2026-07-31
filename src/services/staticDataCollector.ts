c2bc8a93e642ce863d34f92f775853a05dff1831
yuri_rodnov <somewaystodo@yandex.ru>
2026-08-01 00:18:21 +0300

    add static cpu

--- src/services/staticDataCollector.ts
+++ src/services/staticDataCollector.ts
@@ -1,11 +1,17 @@
 // src/services/dataCollector.ts
-
+import { StaticCommon } from "../types/dataTypes";
 import si from "systeminformation";
 
-export const getStaticInformation = async () => {
+export const getStaticInformation = async (): Promise<StaticCommon> => {
   const cpu = await si.cpu();
 
-  if (cpu) {
-    return cpu;
-  }
+  const cpuInfo = {
+    modelName: cpu.brand,
+  };
+
+  const commonInfo: StaticCommon = {
+    cpu: cpuInfo,
+  };
+
+  return commonInfo;
 };
--- src/types/dataTypes.ts
+++ src/types/dataTypes.ts
@@ -1,62 +1,79 @@
-// src/types/dataTypes.ts
+// // src/types/dataTypes.ts
 
-export interface SystemInfo {
-  hostname: string;
-  os: string;
-  arch: string;
-  uptime: number;
+// export interface SystemInfo {
+//   hostname: string;
+//   os: string;
+//   arch: string;
+//   uptime: number;
+// }
+
+// export interface CpuData {
+//   model: string;
+//   physicalCores: number;
+//   logicalCores: number;
+//   usagePercent: number;
+//   clockSpeedMhz: number;
+// }
+
+// export interface MemoryData {
+//   totalBytes: number;
+//   usedBytes: number;
+//   availableBytes: number;
+//   usagePercent: number;
+//   swapTotalBytes: number;
+//   swapUsedBytes: number;
+// }
+
+// export interface DiskData {
+//   mountPoint: string;
+//   fstype: string;
+//   totalBytes: number;
+//   usedBytes: number;
+//   freeBytes: number;
+//   usagePercent: number;
+//   readBytesPerSec: number;
+//   writeBytesPerSec: number;
+// }
+
+// export interface NetworkData {
+//   interfaceName: string;
+//   ip: string;
+//   mac: string;
+//   totalBytesSent: number;
+//   totalBytesRecv: number;
+//   bytesSentPerSec: number;
+//   bytesRecvPerSec: number;
+// }
+
+// export interface ProcessData {
+//   pid: number;
+//   name: string;
+//   cpuPercent: number;
+//   memPercent: number;
+// }
+
+// export interface DashboardData {
+//   system: SystemInfo;
+//   cpu: CpuData;
+//   memory: MemoryData;
+//   disks: DiskData[];
+//   networks: NetworkData[];
+//   topProcesses: ProcessData[];
+// }
+
+// Static data
+
+interface StaticCPU {
+  modelName: string;
+}
+interface StaticMemory {}
+interface StaticHost {}
+interface StaticStorage {}
+export interface StaticCommon {
+  cpu: StaticCPU;
 }
 
-export interface CpuData {
-  model: string;
-  physicalCores: number;
-  logicalCores: number;
-  usagePercent: number;
-  clockSpeedMhz: number;
-}
+// Dynamic data
 
-export interface MemoryData {
-  totalBytes: number;
-  usedBytes: number;
-  availableBytes: number;
-  usagePercent: number;
-  swapTotalBytes: number;
-  swapUsedBytes: number;
-}
-
-export interface DiskData {
-  mountPoint: string;
-  fstype: string;
-  totalBytes: number;
-  usedBytes: number;
-  freeBytes: number;
-  usagePercent: number;
-  readBytesPerSec: number;
-  writeBytesPerSec: number;
-}
-
-export interface NetworkData {
-  interfaceName: string;
-  ip: string;
-  mac: string;
-  totalBytesSent: number;
-  totalBytesRecv: number;
-  bytesSentPerSec: number;
-  bytesRecvPerSec: number;
-}
-
-export interface ProcessData {
-  pid: number;
-  name: string;
-  cpuPercent: number;
-  memPercent: number;
-}
-
-export interface DashboardData {
-  system: SystemInfo;
-  cpu: CpuData;
-  memory: MemoryData;
-  disks: DiskData[];
-  networks: NetworkData[];
-  topProcesses: ProcessData[];
-}
+interface DynamicCPU {}
+interface DynamicCommon {}
